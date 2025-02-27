8f6248b8e3ec723afbfb30d40637b19bf03786d4
GritLilan <lilangf123@163.com>
2025-02-27 16:55:30 +0800

    借教室题解

--- 二分/牛客_借教室/牛客_借教室.cpp
+++ 二分/牛客_借教室/牛客_借教室.cpp
@@ -12,13 +12,18 @@ void add(int l, int r, int c){
 }
 
 bool check(int k){
+    //由于在check里面进行了增减操作，之后没有恢复数组
+    //所以我们用b[]数组作为临时数组，只在check里生效
+    for(int i = 1; i <= n; i++) b[i] = a[i];
     for(int i = 1; i <= k; i++){
-        add(s[i], t[i], d[i]);
+        add(s[i], t[i], -d[i]);
     }
 
+    int sum = 0;
     for(int i = 1; i <= n; i++){
-        a[i] = b[i] + a[i - 1];
-        if(a[i] < 0) return true;
+        // b[i] = b[i] + b[i - 1];
+        sum += b[i];
+        if(sum < 0) return true;
     }
 
     return false;
@@ -29,9 +34,14 @@ bool check(int k){
 int main(){
     cin >> n >> m;
     for(int i = 1; i <= n; i++){
-        cin >> a[i]; b[i] = a[i] - a[i - 1];
+        cin >> a[i]; 
+        //a[i] = a[i] - a[i - 1];
+        //用上面这一行将a[]转换成自己的差分数组显然是不行的
     }
     for(int i = 1; i <= m; i++) cin >> d[i] >> s[i] >> t[i];
+
+    //注意：反向倒着处理差分数组(因为是自身赋值)
+    for(int i = n; i >0; i--) a[i] -= a[i - 1];
 
     int l = 1, r = m;
     while(l < r){
@@ -40,8 +50,8 @@ int main(){
         else l = mid + 1;
     }
 
-
-    cout << l << endl;
+    if(check(l)) cout << l << endl;
+    else cout << 0 << endl;
 
     return 0;
 }
