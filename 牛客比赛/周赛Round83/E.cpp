fe3c73a6d329f00d13993c6fbb523b80f6d2ac3a
GritLilan <lilangf123@163.com>
2025-03-03 15:10:38 +0800

    牛客周赛83E题

--- 牛客比赛/周赛Round83/E.cpp
+++ 牛客比赛/周赛Round83/E.cpp
@@ -1,38 +1,31 @@
 #include<bits/stdc++.h>
 #define int long long
 using namespace std;
+typedef long long ll;
 const int N = 1e4 + 5;
+const int INF = -1145141919810;
 
 int n,k;
 int a[N];
-int dp[N];
-int sum = 0;
-int res = 0;
-
-void dfs(int u, int index){
-    if(u == k){
-        res = max(res, sum);
-        return;
-    }
-    if(u < k && index ==n) return;
-
-    for(int i = 1; i <= 6; i++){
-        int nx = index + i;
-        if(nx > n) continue;
-        sum += a[nx];
-        dfs(u + 1, nx);
-        sum -= a[nx];
-    }
-}
+int dp[N][1005];
+int res = INF;
 
 signed main(){
     cin >> n >> k;
+    memset(dp, INF, sizeof dp);
     for(int i = 1; i <= n; i++){
         cin >> a[i];
     }
+    dp[0][0] = 0;
 
-    dfs(0, 0);
-
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= min(i, k); j++){
+            for(int p = max((ll)0, i - 6); p < i; p++){
+                dp[i][j] = max(dp[i][j], dp[p][j - 1] + a[i]);
+            }
+        }
+        res = max(res, dp[i][k]);
+    }
 
     cout << res << endl;
 
