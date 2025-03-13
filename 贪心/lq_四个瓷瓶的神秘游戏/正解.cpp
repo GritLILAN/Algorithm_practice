d029ebb09d7be63499e9bffb83be2b01a7b9a2d4
GritLilan <lilangf123@163.com>
2025-03-13 11:56:38 +0800

    蓝桥-四个瓷瓶的神秘游戏-正解

--- 贪心/lq_四个瓷瓶的神秘游戏/正解.cpp
+++ 贪心/lq_四个瓷瓶的神秘游戏/正解.cpp
@@ -1,11 +1,38 @@
 #include<bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main(){
+int a[4];
+int res = 0;
 
+int check(){
+    if(a[0] == a[1] && a[1] == a[2] && a[2] == a[3]) return 1;
+    if(a[1] == a[2] && a[2] == a[3]) return 1;
+    if(a[0] == a[1] && a[1] == a[2]) return 2;
+    return 0;
+}
 
+signed main(){
+    for(int i = 0; i < 4; i++) cin >> a[i];
+    sort(a, a + 4);
 
+    if(check() == 1){
+        res += a[0];
+        res += 2 * a[3];
+    }
+    else if(check() == 2){
+        res += a[3];
+        res += 2 * a[0];
+    }
+    else{
+        a[3] += a[0] * 2;
+        res += a[3];
+        a[1] -= a[0];
+        res += a[1]/3 * 3;
+        if(a[1] % 3 == 2) res++;
+    }
 
+    cout << res << endl;
 
 
     return 0;
