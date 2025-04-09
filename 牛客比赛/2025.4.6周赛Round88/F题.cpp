b3e6d6467871b8dfb30fb7ef2eb63ddaf04ffb02
GritLilan <lilangf123@163.com>
2025-04-09 16:24:10 +0800

    Round88F题(双指针贪心构造)

--- 牛客比赛/2025.4.6周赛Round88/F题.cpp
+++ 牛客比赛/2025.4.6周赛Round88/F题.cpp
@@ -1,39 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> PII;
 const int N = 2e5 + 5;
 
 int n;
 int a[N];
-int sum;
-queue<PII> q;
-
-bool check(){
-
-}
-
 
 int main(){
     cin >> n;
-    sum = n * 3;
-    for(int i = 1; i <= n; i++){
-        cin >> a[i];
-        if(a[i]){
-            sum -= a[i];
-            q.push({a[i], i});
+    for(int i = 0; i < n; i++) cin >> a[i];
+    int pm = 0, r = 0;
+    if(a[0] == 0){//特判
+        while(pm < n && a[pm] == 0) pm++;
+        if(pm == n) for(int i = 0; i < pm; i++) a[i] = i + 1;
+        else for(int i = pm - 1; i >= 0; i--){
+            if(a[i + 1] > 1) a[i] = a[i + 1] - 1;
+            else a[i] = a[i + 1] + 1;
         }
     }
 
-    if(!check()){
-        cout << -1 << endl;
-    }
-    else{
-        for(int i = 0; i < n; i++){
-            cout << a[i] << ' ';
+    bool in_sec = false;
+    for(int i = pm; i < n; i++){
+        if(a[i] == 0){
+            if(!in_sec){
+                r = i;
+                while(r < n && a[r] == 0) r++;
+                in_sec = true;
+            }
+            if(a[i - 1] > a[r] && r != n) a[i] = a[i - 1] - 1;
+            else a[i] = a[i - 1] + 1;
         }
+        else in_sec = false;
+    }
+
+
+    bool f = true;
+    for(int i = 1; i < n; i++){
+        if(abs(a[i] - a[i - 1]) != 1) f = false;
+        if(!f)break;
+    }
+
+    if(!f) cout << -1 << endl;
+    else{
+        for(int i = 0; i < n; i++) cout << a[i] << ' ';
         cout << endl;
     }
-
-
+    
     return 0;
 }
